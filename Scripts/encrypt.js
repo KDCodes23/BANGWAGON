4dcfbb0b21fffd2b6c208d0851e0bc67df7dc313
Muhammad <119015633+MuhammadElsoukkary1@users.noreply.github.com>
2025-03-02 01:03:33 -0500

    Adding in a bunch of logic for encryption will keep adding

--- Scripts/encrypt.js
+++ Scripts/encrypt.js
@@ -25,6 +25,47 @@ async function generateAndImportKey()
   
   generateAndImportKey();
 
+
+
+
+async function AddToEncryptFile(iv, key)
+{
+    const fileName = "Key&IV.enc"; 
+    var filedata = Buffer.concat([Buffer.from(iv), Buffer.from(key)]).toString('base64');
+    var fs = require('fs');
+    if (fs.existsSync(fileName))
+    {
+        // Append to file
+        fs.appendFile(fileName, filedata, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        // Append to file
+        
+    }
+    else 
+    {
+        // Create file
+        fs.writeFile(fileName, filedata, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+          
+    }
+
+    fs.close();
+
+    var data = fs.readFile(fileName, 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log(data);
+    });
+    crypto.subtle.importKey.iv = data[0];
+    const Key = ge1KMHeKsFthAnKX6aI5eGFQcFhpCc2qwwCFj3/yaN0DfkQQjvN3c8z6XhxYuUJj
+    crypto.subtle.encrypt(AES-CBC,data);
+
+
+}
+
 // Example usage
 encryptData("Hello, World!", "your-secret-key-32bytes")
     .then(console.log)
